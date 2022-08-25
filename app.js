c37f6a3bf7dc1f719fede7f5d7b7fd725e01330c
Sanidhya Mahna <sanidhya.mahna@gmail.com>
2022-08-25 12:45:20 +0530

    bugs resolved :bug:

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var bodyParser = require("body-parser");
+
 var mongoose = require("mongoose");
 var methodOverride = require("method-override");
 var passport = require("passport");
@@ -21,7 +21,7 @@ mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(flash());
 
